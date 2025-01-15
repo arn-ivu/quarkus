517346d266cf96b1cf9094684f2efee6f7accf32
Hannah Arndt <arn@ivu.de>
2025-01-15 16:23:49 +0100

    reproducer: quarkus-unit-test has classloader issue with parameterized tests

---
+++ extensions/opentelemetry/deployment/src/test/java/io/quarkus/opentelemetry/deployment/traces/ClassMismatchTest_Control.java
@@ -0,0 +1,28 @@
+package io.quarkus.opentelemetry.deployment.traces;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+public class ClassMismatchTest_Control {
+
+    @ParameterizedTest
+    @MethodSource("provideEnumValues")
+    void test(MyEnum myEnum) {
+        assertThat(myEnum).isNotNull();
+    }
+
+    private static enum MyEnum {
+        VALUE1, VALUE2
+    }
+
+    public static Stream<Arguments> provideEnumValues() {
+        return Stream.of(Arguments.of(MyEnum.VALUE1), Arguments.of(MyEnum.VALUE2));
+    }
+
+
+}
